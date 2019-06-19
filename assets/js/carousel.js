e2f5ccd4fd363d62e2c433a60957f66ad115046b
Edson Jair Vasquez <ejair2709@gmail.com>
2019-06-19 01:34:10 -0500

    Se agregan logos,img y datos de ADMA

---
+++ assets/js/carousel.js
@@ -0,0 +1,41 @@
+var positions = [2, 3];
+var buttons = document.getElementById("buttons");
+for (pos of positions) {
+  buttons.innerHTML += `<li class="page-item ">
+         <a class="page-link" href="#galeria${pos}"data-toggle="tab" role="tablist">${pos}</a>
+       </li>`;
+}
+
+var images = [1, 2, 3,4,5,6,7,8,9];
+var gallery = document.getElementById("gallery");
+for (image of images) {
+  gallery.innerHTML += ` 
+        <div class="card">
+            <a href="#" data-toggle="modal" data-target="#id${image}">
+                <img src="assets/img/portfolio/${image}.jpg" alt="" class="card-img-top" >
+            </a>
+        </div>`;
+}
+
+var images = [10,11,12,13,14, 15, 16,17,18,19];
+var gallery2 = document.getElementById("gallery2");
+for (image of images) {
+  gallery2.innerHTML += ` 
+                  <div class="card">
+                      <a href="#" data-toggle="modal" data-target="#id${image}">
+                          <img src="assets/img/portfolio/${image}.jpg" alt="" class="card-img-top" >
+                      </a>
+                  </div>
+                 `;
+}
+var images = [20,21,22,23,24,25];
+var gallery3 = document.getElementById("gallery3");
+for (image of images) {
+  gallery3.innerHTML += ` 
+                           <div class="card">
+                               <a href="#" data-toggle="modal" data-target="#id${image}">
+                                   <img src="assets/img/portfolio/${image}.jpg" alt="" class="card-img-top" >
+                               </a>
+                           </div>
+                           `;
+}
